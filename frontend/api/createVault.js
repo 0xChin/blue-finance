2b017f10cfd9e2beea954e11d0827cab154d8d24
NahWe <92744816+NahWe@users.noreply.github.com>
2024-08-09 23:22:19 -0300

    feat: add api

---
+++ frontend/api/createVault.js
@@ -0,0 +1,47 @@
+import { createPublicClient, createWalletClient, http } from 'viem';
+import { getContract } from 'viem';
+import { ABI, CONTRACT_ADDRESS } from '../../vaultConfig';
+
+
+const publicClient = createPublicClient({
+  transport: http(),
+  chain: {
+    id: 1, 
+    name: 'Ethereum',
+  },
+});
+
+const walletClient = createWalletClient({
+  transport: http(),
+  chain: {
+    id: 1,
+    name: 'Ethereum',
+  },
+  privateKey: process.env.PRIVATE_KEY,
+});
+
+
+const contract = getContract({
+  address: CONTRACT_ADDRESS,
+  abi: ABI,
+  client: walletClient,
+})
+
+export default async function handler(req, res) {
+  const { method, params } = req.body;
+  
+  try {
+    let result;
+
+    switch (method) {
+      case 'createERC4626':
+        const { asset, minHealthFactor, maxHealthFactor } = params;
+         result = await factoryContract.createERC4626(asset, minHealthFactor, maxHealthFactor);
+        break;
+    }
+
+    res.status(200).json({ success: true, result });
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message });
+  }
+}
